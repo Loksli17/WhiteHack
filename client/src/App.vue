3db5a6ab9274a5eaec9beecfbea214e039558731
Alexander <sansanichick@gmail.com>
2022-03-22 20:17:08 +1000

    font

--- client/src/App.vue
+++ client/src/App.vue
@@ -16,8 +16,11 @@
 </template>
 
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700&family=Pacifico&display=swap');
+
     html, body {
         margin: 0;
         padding: 0;
+        font-family: "Montserrat";
     }
 </style>
