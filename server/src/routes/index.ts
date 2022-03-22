7d8609fb300d1c314ec67bf859561d7ef6656c0c
Loksli17 <ami0504@mail.ru>
2022-03-22 13:49:52 +1000

    APIs

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,6 +3,7 @@ import { Router as ExpressRouter } from 'express';
 
 import ExampleController from '../controllers/ExampleController';
 import EventController   from '../controllers/EventController';
+import UserController    from '../controllers/UserController';
 
 
 export default class Router {
@@ -13,6 +14,7 @@ export default class Router {
         
         this.router.use('/example',  ExampleController.routes());
         this.router.use('/event',    EventController.routes());
+        this.router.use('/user',     UserController.routes());
 
         return this.router;
     }
